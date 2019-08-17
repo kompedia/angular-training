387a4dd547cfcebc3b1db8a63a21d2a15f6c5292
Jakub Barczyk <Jakub Barczyk>
2019-08-17 15:04:39 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
